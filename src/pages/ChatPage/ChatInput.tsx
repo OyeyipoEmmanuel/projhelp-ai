a640b6e79b7d7e0344937a6b4bb259a994e94866
Emmanuel <emmasco0207@gmail.com>
2025-11-27 00:23:54 +0100

    feat: chat with ai improvements

--- src/pages/ChatPage/ChatInput.tsx
+++ src/pages/ChatPage/ChatInput.tsx
@@ -2,16 +2,22 @@ import { useState, type FormEvent } from "react";
 import { FiSend } from "react-icons/fi";
 import { addUserChat } from "../../api/chats/addUserChat";
 import { askAi } from "../../api/chats/askAI";
+import { useGetHistory } from "../../api/chats/getHistory";
+import LoadingAiResponse from "../../components/Loading/LoadingAiResponse";
 
 const ChatInput = () => {
     const [input, setInput] = useState<string>("")
     const [awaitingBotRes, setAwaitingBotRes] = useState<boolean>(false)
 
+    const { message: prevMsg } = useGetHistory();
+
+
     let isSending = false
 
     const handleUserPrompt = async (e: FormEvent) => {
         e.preventDefault()
-        
+
+        setInput('');
         if (isSending) return
 
 
@@ -23,8 +29,8 @@ const ChatInput = () => {
 
             // Ask ai
             setAwaitingBotRes(true)
-            
-            await askAi(input)
+
+            await askAi(input, prevMsg)
 
             setAwaitingBotRes(false)
 
@@ -33,7 +39,7 @@ const ChatInput = () => {
         }
         finally {
             setAwaitingBotRes(false);
-            setInput('');
+
             isSending = false;
         }
 
@@ -43,29 +49,32 @@ const ChatInput = () => {
     // console.log(awaitingBotRes)
 
     return (
-        <form onSubmit={handleUserPrompt} className=" rounded-xl border border-[#5737f874] shadow-md flex flex-row items-center justify-between space-x-4 px-3">
-            <textarea
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="Describe your project idea..."
-                className="w-[90%] flex-1 resize-none bg-transparent border-none outline-none text-slate-900 placeholder:text-slate-400 min-h-[70px] max-h-[100px] py-3 px-2"
-                rows={1}
-                required
-                style={{
-                    height: 'auto',
-                    minHeight: '70px',
-                }}
-                onInput={(e) => {
-                    const target = e.target as HTMLTextAreaElement;
-                    target.style.height = 'auto';
-                    target.style.height = target.scrollHeight + 'px';
-                }}
-            />
+        <>
+            {awaitingBotRes && <LoadingAiResponse />}
+            <form onSubmit={handleUserPrompt} className=" rounded-xl border border-[#5737f874] shadow-md flex flex-row items-center justify-between space-x-4 px-3">
+                <textarea
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Describe your project idea..."
+                    className="w-[90%] flex-1 resize-none bg-transparent border-none outline-none text-slate-900 placeholder:text-slate-400 min-h-[70px] max-h-[100px] py-3 px-2"
+                    rows={1}
+                    required
+                    style={{
+                        height: 'auto',
+                        minHeight: '70px',
+                    }}
+                    onInput={(e) => {
+                        const target = e.target as HTMLTextAreaElement;
+                        target.style.height = 'auto';
+                        target.style.height = target.scrollHeight + 'px';
+                    }}
+                />
 
-            <button type="submit" disabled={isSending || input.trim().length == 0} className="bg-[#5837F8] p-3 rounded-lg text-center hover:opacity-80 hover:transition-all hover:duration-200 cursor-pointer disabled:opacity-30 disabled:cursor-not-allowed">
-                <FiSend className="text-lg text-white" />
-            </button>
-        </form>
+                <button type="submit" disabled={isSending || input.trim().length == 0} className="bg-[#5837F8] p-3 rounded-lg text-center hover:opacity-80 hover:transition-all hover:duration-200 cursor-pointer disabled:opacity-30 disabled:cursor-not-allowed">
+                    <FiSend className="text-lg text-white" />
+                </button>
+            </form>
+        </>
     )
 }
 
