35bd46682757b7ae1543ac87eaddbd078afed477
Emmanuel <emmasco0207@gmail.com>
2025-11-27 00:25:27 +0100

    feat: chat with ai improvements

---
+++ src/components/Loading/LoadingAiResponse.tsx
@@ -0,0 +1,16 @@
+import thinkingGif from "../../assets/thinking gif.gif"
+
+const LoadingAiResponse = () => {
+    return (
+        <section className='absolute top-0 mx-auto left-0 w-full flex items-center justify-center  text-white '>
+            <div className='w-full min-h-screen absolute top-0 left-0 bg-black/50'></div>
+            <div className='bg-[#5837F8] px-6 py-2 rounded-md shadow-xl z-40 absolute top-10 animate-pulse'>Thinking...</div>
+            <div className="flex items-center justify-center h-screen">
+                <img src={thinkingGif} alt="thinking" className="w-[60%] rounded-xl" />
+            </div>
+            
+        </section>
+    )
+}
+
+export default LoadingAiResponse
