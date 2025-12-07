55434411753accca4c0f1eb6509edeeba6cf36c7
Emmanuel <emmasco0207@gmail.com>
2025-12-08 00:10:54 +0100

    Optimization type shii

--- src/pages/LandingPg/LandingPg.tsx
+++ src/pages/LandingPg/LandingPg.tsx
@@ -10,10 +10,11 @@ const LandingPg = () => {
 
     return (
         <main className="relative flex flex-col items-center justify-between space-y-4 px-4 pt-6 md:flex-row md:space-y-0 md:space-x-6 md:py-24">
-            {/* Spinning square */}
-            <section className="absolute w-18 h-18 border border-purple-400 rounded-md top-[10%] md:left-[50%] right-12 animate-spin [animation-duration:8s]"></section>
-
-            <section className="md:w-[50%] md:flex md:flex-col md:space-y-6">
+            {/* Spinning square */} 
+            <div className="absolute w-20 h-20 top-[10%] md:left-[50%] right-12">
+                <section className=" w-18 h-18 border border-purple-400 rounded-md  animate-spin [animation-duration:8s]"></section>
+            </div>
+            <section className="w-full md:w-[50%] md:flex md:flex-col md:space-y-6">
                 <div className="bg-[#E9E8FF] text-center rounded-full px-3 py-1 text-[#702FED] border border-[#DDD6FF] md:w-fit">
                     ✨ Powered by Gemini 2.0 Flash
                 </div>
@@ -24,25 +25,25 @@ const LandingPg = () => {
                 </div>
 
                 <div className="flex flex-col items-center justify-center space-y-2 mt-4 md:flex-row md:space-y-0 md:space-x-4">
-                    <CTAbuttons btnName="Get Started" className="w-full bg-[#5837F8] text-white" onClick={()=>navigate("/chat")} />
+                    <CTAbuttons btnName="Get Started" className="w-full bg-[#5837F8] text-white" onClick={() => navigate("/chat")} />
                     <CTAbuttons btnName="Watch Demo" className="w-full bg-transparent border border-black/20 text-black" onClick={() => alert("Coming Soon!")} />
                 </div>
 
-                <div className="mt-3 flex flex-row justify-center items-center space-x-3 md:justify-start">
+                <div className="mt-3 flex flex-col justify-center space-y-1 md:space-y-0 md:space-x-3 md:justify-start md:flex-row">
                     <span className="flex flex-row items-center space-x-1">
                         <div className="w-2 h-2 bg-green-500 rounded-full "></div>
                         <p className="text-black/70 text-[15px]">No Login Required</p>
                     </span>
 
-                    <span className="flex flex-row items-center space-x-1">
+                    <span className="flex flex-row items-center space-x-1 justify-end">
                         <div className="w-2 h-2 bg-green-500 rounded-full "></div>
                         <p className="text-black/70 text-[15px]">No Credit Cart Required</p>
                     </span>
                 </div>
             </section>
 
-            <section className="bg-white shadow-xl mb-2 rounded-xl md:w-[45%]">
-                <img src={landingPgImg} alt="projhelp-ai" className="p-5 rounded-4xl " />
+            <section className="w-full bg-white shadow-xl mb-2 rounded-xl md:w-[45%]">
+                <img src={landingPgImg} alt="projhelp-ai" className="p-5 rounded-4xl " loading="eager" fetchPriority="high" />
             </section>
         </main>
     )
